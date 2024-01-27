123ba1ed7f2ecaa8ec214c3dd051a5a4d52359e1
w3schools-test <test@w3schools.com>
2024-01-27 16:10:32 +0530

    First program

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = scan.nextLine();
+		System.out.print("My name is: "+name);
+	}
+
+}
